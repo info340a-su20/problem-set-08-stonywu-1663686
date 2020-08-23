1935342c28926214b59c5fa0ea2d20e12c86761f
Stony Wu <stonywu@uw.edu>
2020-08-23 11:57:43 -0400

    finish problem-a

--- problem-a/src/App.js
+++ problem-a/src/App.js
@@ -5,4 +5,73 @@ const EXAMPLE_SENATORS = [
   { id: 'M001111', name: 'Patty Murray', state: 'WA', party: 'Democrat', phone: '202-224-2621', twitter: 'PattyMurray' }
 ];
 
-/* Your code goes here */
+export class App extends React.Component {
+  render() {
+    let senators = this.props.senators;
+    return (
+      <div className="container">
+        <h1>
+        US Senators 2019
+        </h1>
+        <SenatorTable senators={senators}/>
+      </div>
+    );
+  }
+}
+
+export class SenatorTable extends React.Component {
+  render() {
+    let senatorRows = [];
+    let senators = this.props.senators;
+    for (let each of senators) {
+      senatorRows.push((<SenatorRow key={each.name} senator={each}/>));
+    }
+    return (
+      <table className="table table-bordered">
+        <TableHeader cols={['Name', 'State', 'Phone', 'Twitter']} />
+        <tbody>
+          {senatorRows}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export class TableHeader extends React.Component {
+  render() {
+    let headCols = this.props.cols;
+    return (
+      <thead>
+        <tr>
+          {headCols.map(colsTh)}
+        </tr>
+      </thead>
+    );
+  }
+}
+
+export class SenatorRow extends React.Component {
+  render() {
+    let senator = this.props.senator;
+    return (
+      <tr>
+        <td>
+          {senator.name}
+        </td>
+        <td>
+          {(senator.party.substring(0, 1) + " - " + senator.state)}
+        </td>
+        <td>
+          <a href={"tel:" + senator.phone}>{senator.phone}</a>
+        </td>
+        <td>
+        <a href={"https://twitter.com/" + senator.twitter}>{"@" + senator.twitter}</a>
+        </td>
+      </tr>
+    );
+  }
+}
+
+function colsTh(col) {
+  return (<th key={col}>{col}</th>);
+}
--- problem-a/src/index.js
+++ problem-a/src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { App } from "./App";
 
 //render the App component here!
+ReactDOM.render(<App/>, document.getElementById("root"));
